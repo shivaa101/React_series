684e37113612e49e5e5fb3762f926a548f34f90f
Shivam Bhagwat <shivam.bhagwat@mitaoe.ac.in>
2024-06-27 11:01:42 +0530

    Initial commit

---
+++ reactbasic/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Name from '../src/name.jsx'
+function App() {
+  
+
+  return (
+    <>
+<Name />
+    </>
+  )
+}
+
+export default App
---
+++ reactbasic/src/name.jsx
@@ -0,0 +1,11 @@
+function name() {
+  
+
+    return (
+      
+        <h1>My Name is : Shivam Bhagwat</h1>
+       
+    )
+  }
+  
+  export default name
